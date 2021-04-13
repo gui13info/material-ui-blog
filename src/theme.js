ebf6790ee63dc6a63a855827c0ddc57d3694a0f4
Guilherme Matos <marques.guilherme0067@gmail.com>
2021-04-13 16:46:44 -0300

    13/03

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#764ABC"
+        },
+        background: {
+            default: "#fff"
+        }
+    }
+})
+
+export default theme;
